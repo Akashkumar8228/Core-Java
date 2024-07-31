0bc3c529d7bf322a498c29928b0977275f02202b
Akash kumar <akashkumargupta8228@gmail.com>
2024-07-31 23:07:12 +0530

    code

---
+++ Array/P1.java
@@ -0,0 +1,31 @@
+  /*
+ i/p array of size n
+ Step 1 = CREATE A NEW ARRAY OF SIZE N
+ STEP 2 = COPY FIRST ELEMENT FROM ARRAY INTO NEW ARRAY
+ STEP 3 = CRAETE COUNTER K AND INTIALIZE TO ONE
+            (K = IT REPRESENT NUMBER OF UNIQUE ELEMENTS INSERTED INTO NEW ARRAY)
+ STEP 4 = ADD REMAINING UNIQUE ELEMENTS INTO NEW ARRAY
+ STEP 5 = TRIM THE SIZE OF NEW ARRAY TO K
+
+
+ */
+  
+package Array;
+
+public class P1 {
+
+    public static int[] removeDublicates(int [] array)
+    {
+        int [] newArray = new int[array.length];
+        newArray[0] = array[0];
+
+        //S3: create k and initialize 1
+        int k = 1;
+        return newArray;
+        
+    }
+  public static void main(String[] args) {
+    
+
+  }
+}
